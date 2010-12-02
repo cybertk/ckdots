5b0cc987b2e51c4707b89ecf7977fd92dce83213
Kyan He <kyan.ql.he@gmail.com>
2010-12-02 18:46:17 +0800

    func, fix find precedence in cgrep

--- bash_profile.d/grep.func.sh
+++ bash_profile.d/grep.func.sh
@@ -1,6 +1,6 @@
 function cgrep()
 {
-    find . -type f -name "*\.c*" -or -name "*\.h" -print0 | xargs -0 grep --color -n "$@"
+    find . -type f \( -name "*\.c*" -or -name "*\.h" \) -print0 | xargs -0 grep --color -n "$@"
 }
 
 function jgrep()
