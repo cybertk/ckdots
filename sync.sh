c5180c9dff3d25739a27ab960a16ae8ff7d0f219
Kyan He <kyan.ql.he@gmail.com>
2011-12-15 11:15:24 +0800

    Bug fix: Syncing files whose path contains spaces

--- sync.sh
+++ sync.sh
@@ -2,18 +2,19 @@
 # @param $2, dst, files in real dir
 function sync() {
 
-if [ -d $1 ];
+if [ -d "$1" ];
 then
     # Create directory if un-exist.
-    if [ ! -e $2 ];
+    if [ ! -e "$2" ];
     then
         echo "Syncing $2"
-        [ -n "$dryrun" ] || mkdir $2
+        [ -n "$dryrun" ] || mkdir "$2"
     fi
 
     for f in $(/bin/ls $1);
     do
-        sync $1/$f $2/$f
+        # NOTE: Quote var with "" to avoid space(Path contains space) issue.
+        sync "$1/$f" "$2/$f"
     done
 else
     # Won't sync .gitignore
@@ -23,17 +24,17 @@ else
     then
         if [ -f "$2" ];
         then
-            diff -Nur $2 $1
+            diff -Nur "$2" "$1"
         else
-            diff -Nur /dev/null $1
+            diff -Nur /dev/null "$1"
         fi
     else
-        if diff $1 $2  2>&1 1>&/dev/null;
+        if diff "$1" "$2"  2>&1 1>&/dev/null;
         then
             echo "[$1] Latest" >/dev/null
         else
             echo "Syncing $2"
-            [ -n "$dryrun" ] || /bin/cp $1 $2
+            [ -n "$dryrun" ] || /bin/cp "$1" "$2"
         fi
     fi
 fi
