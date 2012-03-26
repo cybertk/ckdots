9670e76a13f9ee311624f672ba62d4232a4bb724
Kyan He <kyan.ql.he@gmail.com>
2012-03-26 17:07:48 +0800

    grep: Add xgrep to grep xml files.

--- bash_profile.d/grep.func.sh
+++ bash_profile.d/grep.func.sh
@@ -7,3 +7,8 @@ function jgrep()
 {
     find . -type f -name "*\.java" -print0 | xargs -0 grep --color -n "$@"
 }
+
+function xgrep()
+{
+    find . -type f -name "*\.xml" -print0 | xargs -0 grep --color -n "$@"
+}
