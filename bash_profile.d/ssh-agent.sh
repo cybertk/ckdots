0fd5110fbe2fee626e18ff913cdf0d9dc2e1ac60
Kyan He <kyan.ql.he@gmail.com>
2012-05-30 13:13:34 +0800

    ssh-agent.sh: Rewrite, add pid support.

--- bash_profile.d/ssh-agent.sh
+++ bash_profile.d/ssh-agent.sh
@@ -1,15 +1,31 @@
-LOCAL_SSH_AUTH=/tmp/ssh-agent.sock
+_SSH_AUTH_SOCK=/tmp/ssh-agent.sock
+_SSH_PID_FILE=/tmp/ssh-agent.pid
+_SSH_AGENT_PID=$(cat $_SSH_PID_FILE 2>/dev/null)
 
 # validate SSH_AUTH socket.
-if [ -S "$LOCAL_SSH_AUTH" ];
-then
-    # ssh-agent is running now.
-    export SSH_AUTH_SOCK=$LOCAL_SSH_AUTH
+if [[ -S "$_SSH_AUTH_SOCK" && -n $_SSH_AGENT_PID
+    && -d /proc/$_SSH_AGNET_PID ]]; then
 
-    # TODO: export $SSH_AGENT_PID
+    # ssh-agent is already running now.
+    export SSH_AUTH_SOCK=$_SSH_AUTH_SOCK
+    export SSH_AGENT_PID=$_SSH_AGENT_PID
 else
-    eval ssh-agent -a $LOCAL_SSH_AUTH >/dev/null
-    ssh-add
+
+    # Spawn a new ssh-agent
+    eval `ssh-agent -a $_SSH_AUTH_SOCK` >/dev/null
+    if [[ -z $SSH_AGENT_PID ]]; then
+
+        echo Failed to start ssh-agent.
+    else
+
+        # Save pid.
+        echo $SSH_AGENT_PID >$_SSH_PID_FILE
+
+        echo Trying to add keys.
+        ssh-add
+    fi
 fi
 
-unset LOCAL_SSH_AUTH
+unset _SSH_AUTH_SOCK
+unset _SSH_AGENT_PID
+unset _SSH_PID_FILE
