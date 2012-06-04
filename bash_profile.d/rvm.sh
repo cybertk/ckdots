992ee3ad7ebe9e92161c5a8705a19fd4ecf8523c
Kyan He <kyan.ql.he@gmail.com>
2012-06-04 14:30:10 +0800

    Add rvm.sh.

---
+++ bash_profile.d/rvm.sh
@@ -0,0 +1,5 @@
+# Source rvm env.
+[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
+
+# Source rvm auto completion.
+[[ -s "$HOME/.rvm/scripts/completion" ]] && source "$HOME/.rvm/scripts/completion"
