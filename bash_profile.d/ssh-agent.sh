cade64e26f643422562f28967411b0c1bfc8e480
Kyan He <kyan.ql.he@gmail.com>
2012-08-19 22:36:47 +0800

    ssh-agent: Fix: ssh-add failed.

--- bash_profile.d/ssh-agent.sh
+++ bash_profile.d/ssh-agent.sh
@@ -3,13 +3,18 @@ _SSH_PID_FILE=/tmp/ssh-agent.pid
 _SSH_AGENT_PID=$(cat $_SSH_PID_FILE 2>/dev/null)
 
 # validate SSH_AUTH socket.
-if [[ -S "$_SSH_AUTH_SOCK" && -n $_SSH_AGENT_PID
-    && -d /proc/$_SSH_AGNET_PID ]]; then
+# validate pid of runing ssh-agent, use /proc/pid/fd to elimate pid=""
+if [[ -S "$_SSH_AUTH_SOCK" &&
+    -d "/proc/$_SSH_AGENT_PID/fd" ]]; then
 
     # ssh-agent is already running now.
     export SSH_AUTH_SOCK=$_SSH_AUTH_SOCK
     export SSH_AGENT_PID=$_SSH_AGENT_PID
 else
+
+    # \todo kill ssh-agent use $_SSH_AUTH_SOCK
+    # Remove $_SSH_AUTH_SOCK to avoid "bind: Address already in use"
+    [[ -S "$_SSH_AUTH_SOCK" ]] && /bin/rm $_SSH_AUTH_SOCK
 
     # Spawn a new ssh-agent
     eval `ssh-agent -a $_SSH_AUTH_SOCK` >/dev/null
