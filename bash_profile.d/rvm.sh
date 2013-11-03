348fc00d46d7f083dfb32f2a598f69e4711c2110
Quanlong He <quanlongx.he@intel.com>
2013-11-03 18:25:36 +0800

    Update rvm profile.

--- bash_profile.d/rvm.sh
+++ bash_profile.d/rvm.sh
@@ -1,4 +1,6 @@
 # Source rvm env.
+export PATH=$HOME/.rvm/bin:$PATH # Add RVM to PATH for scripting
+
 [[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
 
 # Source rvm auto completion.
