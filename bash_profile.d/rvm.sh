2bb839c1f0e81869402b2554ee078e75d7cd5d29
Quanlong He <kyan.ql.he@gmail.com>
2015-11-10 23:31:10 +0800

    [rvm] Replace rvm with rbenv

--- bash_profile.d/rvm.sh
+++
@@ -1,7 +0,0 @@
-# Source rvm env.
-export PATH=$HOME/.rvm/bin:$PATH # Add RVM to PATH for scripting
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm"
-
-# Source rvm auto completion.
-[[ -s "$HOME/.rvm/scripts/completion" ]] && source "$HOME/.rvm/scripts/completion"
