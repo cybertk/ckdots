5ba515e6c01ceb60c4bc46920b987184fc807e00
Quanlong He <kyan.ql.he@gmail.com>
2015-11-10 22:58:40 +0800

    [nvm] Use taobao mirror

--- bash_profile.d/nvm.sh
+++ bash_profile.d/nvm.sh
@@ -3,3 +3,7 @@
 export NVM_DIR=~/.nvm
 
 [[ -s "$(brew --prefix nvm)/nvm.sh" ]] && source "$(brew --prefix nvm)/nvm.sh"
+
+# See https://npm.taobao.org/
+export NVM_NODEJS_ORG_MIRROR=http://npm.taobao.org/mirrors/node
+export NVM_IOJS_ORG_MIRROR=http://npm.taobao.org/mirrors/iojs
