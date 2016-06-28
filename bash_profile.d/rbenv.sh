1bdd5d66bfb6a9b4a02e6abed61a0a397bd8a2e7
Quanlong <kyan.ql.he@gmail.com>
2016-06-28 23:45:13 +0800

    chore(rbenv): Use taobao mirror

--- bash_profile.d/rbenv.sh
+++ bash_profile.d/rbenv.sh
@@ -3,3 +3,5 @@ export RBENV_ROOT=/usr/local/var/rbenv
 
 # To enable shims and autocompletion add to your profile:
 if which rbenv > /dev/null; then eval "$(rbenv init -)"; fi
+
+export RUBY_BUILD_MIRROR_URL=https://ruby.taobao.org/mirrors/ruby
