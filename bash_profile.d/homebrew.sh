5f6e2faa7a8fe773125665bd21f5058d88abf0b6
Quanlong <kyan.ql.he@gmail.com>
2016-07-09 15:13:24 +0800

    feat(homebrew): Support bottle mirror

--- bash_profile.d/homebrew.sh
+++ bash_profile.d/homebrew.sh
@@ -1,2 +1,7 @@
-# Use LUG Bottle Mirror, see https://servers.ustclug.org/2016/01/mirrors-add-homebrew-bottles-source/
-export HOMEBREW_BOTTLE_DOMAIN=https://mirrors.ustc.edu.cn/homebrew-bottles
+# Increase Github rate limit by using api token
+export HOMEBREW_GITHUB_API_TOKEN=$CONFIG_GITHUB_API_TOKEN
+
+# Avialble Bottle Mirrors, select one and add into ~/.config/ck-bots
+# - https://mirrors.ustc.edu.cn/homebrew-bottles
+#   LUG Mirror, see https://servers.ustclug.org/2016/01/mirrors-add-homebrew-bottles-source/
+[[ -n $CONFIG_HOMEBREW_BOTTLE_MIRROR ]] && export HOMEBREW_BOTTLE_DOMAIN=$CONFIG_HOMEBREW_BOTTLE_MIRROR
