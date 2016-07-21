f01cdbdc595371f7cb37aa2157eae566a82d07fa
Quanlong <kyan.ql.he@gmail.com>
2016-07-21 21:06:18 +0800

    refactor: Remove old files

--- Gemfile
+++
@@ -1,3 +0,0 @@
-source "https://rubygems.org"
-
-gem "shlint", :git => 'https://github.com/cybertk/shlint.git'
--- track_empty_dir.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-#
-# Copyright (C) 2011 Kyan <kyan.ql.he@gmail.com>
-#
-find -type d -empty -exec touch {}/.gitkeep \;
