0b7f03722b4985089236d792f2a2d18605cf7597
Quanlong <kyan.ql.he@gmail.com>
2017-04-27 15:53:12 +0800

    feat(plugin/carthage): Access Github with API TOKEN

---
+++ plugins/carthage.sh
@@ -0,0 +1,2 @@
+# Set global Github access token for Carthage CLI, see https://github.com/Carthage/Carthage/pull/605
+[[ -n $CONFIG_CARTHAGE_GITHUB_TOKEN ]] && export GITHUB_ACCESS_TOKEN=$CONFIG_CARTHAGE_GITHUB_TOKEN
